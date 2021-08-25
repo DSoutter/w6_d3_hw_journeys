8fa7682f762a37e18119e90508a7e3ce109b68bf
DSoutter <duncan.soutter@gmail.com>
2021-08-25 17:33:34 +0100

    MVP done

--- models/traveller.js
+++ models/traveller.js
@@ -28,10 +28,19 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {
+    return total + journey.distance
+  }, 0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
+  const newJourneys =  this.journeys.map((journeys) =>{
+    return journeys.transport
+  })
+  // indexOf cycles through the different items in an array and checks if the index is equal to the current index. As indexOf will take the first transport type, when the indices are different, it will return false and not be included in the return... e.g for train #2, indexOf will be 0 and index will be 1.
+  return newJourneys.filter((journey, index) =>{
+    return indexOf(journey) === journey;
+  })
 
 };
 
